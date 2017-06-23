7da15e1cba55a8cc6eb7906f9340fee876aa4a31
ethancwb <ethancwb@foxmail.com>
2017-06-22 20:48:11 -0400

    basic environment setup

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-require ("./test/app.js")(app);
+var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds143340.mlab.com:43340/heroku_vqfxvvh9'; // user yours
+}
+
+var mongoose = require("mongoose");
+mongoose.connect(connectionString);
 
 var port = process.env.PORT || 3000;
 
