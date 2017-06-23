8f2c81753dcea4e1bd6c87baad9c6c61a5503da1
ethancwb <ethancwb@foxmail.com>
2017-06-23 19:20:37 -0400

    view update

--- public/template/controller/user_controller/homepage-controller.view.client.js
+++ public/template/controller/user_controller/homepage-controller.view.client.js
@@ -1,42 +1,26 @@
 (function () {
     angular
         .module('OnlineWebStore')
-        .controller('homepageController', homepageController)
-
-    function homepageController () {
-        var model = this
-        model.fk = (function () {
-            console.log("dgdgsfads")
+        .controller('homepageController', function () {
+            var model = this
+            model.plusSlides = (function (n) {
+                showSlides(slideIndex += n)
+            })
+            function showSlides(n) {
+              var i
+              var slides = document.getElementsByClassName("mySlides")
+              if (n > slides.length) {
+                  slideIndex = 1
+              }
+              if (n < 1) {
+                  slideIndex = slides.length
+              }
+              for (i = 0; i < slides.length; i++) {
+                  slides[i].style.display = "none"
+              }
+              slides[slideIndex-1].style.display = "block"
+            }
+            var slideIndex = 1
+            showSlides(slideIndex)
         })
-        // model.currentSlide = function (n) {
-        //     showSlides(slideIndex = n);
-        // }
-        model.error = "agasdfasdfasdfa"
-        console.log('here')
-        // var slideIndex = 1
-        // showSlides(1)
-        //
-        // function showSlides (n) {
-        //     var i;
-        //     var slides = document.getElementsByClassName("mySlides")
-        //     console.log(slides)
-        //     // var dots = document.getElementsByClassName("dot")
-        //
-        //     if (n > slides.length) {
-        //         slideIndex = 1
-        //     }
-        //     if (n < 1) {
-        //         console.log(slides.length)
-        //         slideIndex = slides.length
-        //     }
-        //     for (i = 0; i < slides.length; i++) {
-        //         slides[i].style.display = "none"
-        //     }
-        //     // for (i = 0; i < dots.length; i++) {
-        //     //     dots[i].className = dots[i].className.replace(" active", "")
-        //     // }
-        //     slides[slideIndex-1].style.display = "block"
-        //     // dots[slideIndex-1].className += " active"
-        // }
-    }
 })()
