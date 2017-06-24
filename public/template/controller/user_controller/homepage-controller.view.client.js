74dddb2e85fbc20f56daa614d120ae14b2abcc4e
lixua <li.xua@husky.neu.edu>
2017-06-24 01:18:46 -0400

    123

--- public/template/controller/user_controller/homepage-controller.view.client.js
+++ public/template/controller/user_controller/homepage-controller.view.client.js
@@ -7,18 +7,18 @@
                 showSlides(slideIndex += n)
             })
             function showSlides(n) {
-              var i
-              var slides = document.getElementsByClassName("mySlides")
-              if (n > slides.length) {
-                  slideIndex = 1
-              }
-              if (n < 1) {
-                  slideIndex = slides.length
-              }
-              for (i = 0; i < slides.length; i++) {
-                  slides[i].style.display = "none"
-              }
-              slides[slideIndex-1].style.display = "block"
+                var i
+                var slides = document.getElementsByClassName("mySlides")
+                if (n > slides.length) {
+                    slideIndex = 1
+                }
+                if (n < 1) {
+                    slideIndex = slides.length
+                }
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none"
+                }
+                slides[slideIndex-1].style.display = "block"
             }
             var slideIndex = 1
             showSlides(slideIndex)
