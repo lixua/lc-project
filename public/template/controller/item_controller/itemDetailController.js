b028823a98c391152c9b0bcbe14ef9ac8838fb8c
ethancwb <ethancwb@foxmail.com>
2017-06-25 19:29:31 -0400

    Item detail

--- public/template/controller/item_controller/itemDetailController.js
+++ public/template/controller/item_controller/itemDetailController.js
@@ -1,7 +1,19 @@
 (function () {
     angular
         .module('OnlineWebStore')
-        .controller('itemDetailController', function () {
-            var model = this
+        .controller('itemDetailController', function ($http, $sce) {
+           var model = this
+
+       model.findItemsByKeywords = findItemsByKeywords
+
+       function findItemsByKeywords(keywords) {
+           return $http.jsonp($sce.trustAsResourceUrl('https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=wenbinch-WenbinXu-PRD-47a8f6fab-de043d20&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + keywords + '&paginationInput.entriesPerPage=10'))
+                       .then(function (response) {
+                           model.items = response.data.findItemsByKeywordsResponse[0].searchResult[0].item
+                           return response.data
+                       })
+       }
+       findItemsByKeywords("Dean's milk")
+       // Fields: currency / name / price / price_with_shipping / shipping_costs / shop_id / shop_name / shop_url / source_id / url
         })
 })()
