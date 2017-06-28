2b99f31d943dae78af5629af7f19e77169ae767b
lixua <li.xua@husky.neu.edu>
2017-06-28 13:55:41 -0400

    user profile debug

--- public/backend-service/user.service.server.js
+++ public/backend-service/user.service.server.js
@@ -11,6 +11,7 @@ var googleConfig = {
     clientID     : process.env.GOOGLE_CLIENT_ID,
     clientSecret : process.env.GOOGLE_CLIENT_SECRET,
     callbackURL  : process.env.GOOGLE_CALLBACK_URL
+
 };
 
 passport.use(new LocalStrategy(localStrategy));
